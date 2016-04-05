83a1e498d3c799220579f2978127ba344cf7f8e0
Dan Kaiser <dskaiser82@gmail.com>
2016-04-05 12:37:53 -0700

    add veriable for YouTube vm.covers mainCtrl.js

--- public/js/mainCtrl.js
+++ public/js/mainCtrl.js
@@ -10,12 +10,17 @@
     spotifyService.index()
       .success(function(results){
         console.log(results)
+          //we name spots and set the api object to entries.items
+          //to easily bind on front-end
           vm.spots = results.entries.items
       }) //end function spotifyService anonymous function
 
       tubeService.index()
         .success(function(results){
           console.log(results)
+          //we name covers and set the api object to results.items
+          //to easily bind on front-end
+            vm.covers = results.items
 
         }) //end function spotifyService anonymous function
   } //end function MainController
